7f906f475f559ed05ad6416b2d4ae849acd398a4
athulantony <athul@entri.me>
2019-07-18 15:34:18 +0530

    Add user input field

--- videoeditor.py
+++ videoeditor.py
@@ -1,5 +1,12 @@
 from moviepy.editor import *
-content = VideoFileClip('class.mp4')
-intro = VideoFileClip('intro.mp4')
+
+#Content video location
+content_loc = input("Enter content video location: ") 
+content=VideoFileClip(content_loc)
+
+#Intro video location
+intro_loc =input("Enter Intro video location: ") 
+intro=VideoFileClip(intro_loc)
+
 finalrender = concatenate_videoclips([intro,content,intro])
 finalrender.write_videofile('finalclass.mp4', codec='libx264')
